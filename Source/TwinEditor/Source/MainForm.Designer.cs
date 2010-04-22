c819a3dd0c938137f7edfe0e633a451dc5682000
Boris Bluntschli <boris@bluntschli.ch>
2010-04-22 22:37:57 +0200

    Added TwinEditor project as a testbed for implementation

---
+++ Source/TwinEditor/Source/MainForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace TwinEditor
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SourceCode = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// SourceCode
+			// 
+			this.SourceCode.Location = new System.Drawing.Point(13, 13);
+			this.SourceCode.Multiline = true;
+			this.SourceCode.Name = "SourceCode";
+			this.SourceCode.Size = new System.Drawing.Size(445, 351);
+			this.SourceCode.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 371);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(445, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Information";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(470, 404);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.SourceCode);
+			this.Name = "MainForm";
+			this.Text = "TwinEditor";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		public System.Windows.Forms.Label label1;
+		public System.Windows.Forms.TextBox SourceCode;
+	}
+}
---
+++ Source/TwinEditor/Source/Program.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+using Lebowski;
+using Lebowski.Net;
+using Lebowski.TextModel;
+using Lebowski.Synchronization.DifferentialSynchronization;
+
+namespace TwinEditor
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Program prog = new Program();
+			prog.Run();
+		}
+		
+		MainForm clientForm;
+		MainForm serverForm;
+		
+		DifferentialSynchronizationStrategy server;
+		DifferentialSynchronizationStrategy client;
+		
+		private void Run()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);		
+			
+			clientForm = new MainForm();
+			clientForm.Text = "Client (1)";
+			
+			serverForm = new MainForm();			
+			serverForm.Text = "Server (0)";
+
+			LocalConnection serverConnection = new LocalConnection();
+			LocalConnection clientConnection = new LocalConnection();
+			LocalProtocol.Connect(serverConnection, clientConnection);
+			
+			ITextContext serverContext = new TextBoxTextContext(serverForm.SourceCode);
+			ITextContext clientContext = new TextBoxTextContext(clientForm.SourceCode);
+			
+			server = new DifferentialSynchronizationStrategy(0, serverContext, clientConnection);
+			server.EnableAutoFlush = true;
+			client = new DifferentialSynchronizationStrategy(1, clientContext, serverConnection);				
+			client.EnableAutoFlush = true;
+			
+			var timer = new System.Timers.Timer(20);
+			timer.Elapsed += delegate { 
+				clientForm.BeginInvoke((Action)UpdateText);
+			};
+			timer.Enabled = true;
+			
+			clientForm.Show();
+			serverForm.Show();
+			
+			Application.Run();			
+		}
+		
+		void UpdateText()
+		{
+			clientForm.label1.Text = String.Format("{0} {1}", client.State, client.HasChanged);
+			serverForm.label1.Text = String.Format("{0} {1}", server.State, server.HasChanged);			
+		}
+		
+	}
+}
