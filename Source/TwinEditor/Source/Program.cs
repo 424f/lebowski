9396f7303b7da062d2d0d4ed45a5d258c427b42f
Boris Bluntschli <boris@bluntschli.ch>
2010-04-30 14:12:08 +0200

    Cleaned up Program.cs

--- Source/TwinEditor/Source/Program.cs
+++ Source/TwinEditor/Source/Program.cs
@@ -13,54 +13,11 @@ namespace TwinEditor
 {
 	internal sealed class Program
 	{
-		/// <summary>
-		/// Program entry point.
-		/// </summary>
 		[STAThread]
 		private static void Main(string[] args)
 		{
 			Program prog = new Program();
 			prog.Run();
-		}
-		
-		MainForm clientForm;
-		MainForm serverForm;
-		
-		DifferentialSynchronizationStrategy server;
-		DifferentialSynchronizationStrategy client;
-
-		private void RunLocalSample()
-		{
-			ServerConnection s = new ServerConnection();
-			System.Console.WriteLine("Server created..");
-			
-			ClientConnection c = new ClientConnection("localhost", ClientConnection.Port);
-			Console.WriteLine("Client connected..");
-
-			clientForm = new MainForm();
-			clientForm.Text = "Client (1)";
-			
-			serverForm = new MainForm();			
-			serverForm.Text = "Server (0)";
-
-			IConnection serverConnection = s; // new LocalConnection();
-			IConnection clientConnection = c; // new LocalConnection();
-			//LocalProtocol.Connect(serverConnection, clientConnection);
-			
-			ITextContext serverContext = new TextEditorTextContext(serverForm.SourceCode);
-			ITextContext clientContext = new TextEditorTextContext(clientForm.SourceCode);
-			
-			server = new DifferentialSynchronizationStrategy(0, serverContext, serverConnection);
-			client = new DifferentialSynchronizationStrategy(1, clientContext, clientConnection);				
-			
-			var timer = new System.Timers.Timer(20);
-			timer.Elapsed += delegate { 
-				clientForm.BeginInvoke((Action)UpdateText);
-			};
-			timer.Enabled = true;
-			
-			clientForm.Show();
-			serverForm.Show();			
 		}
 		
 		private void Run()
@@ -69,59 +26,6 @@ namespace TwinEditor
 			Application.SetCompatibleTextRenderingDefault(false);				
 			
 			MainForm form = new MainForm();
-			
-			/*Console.Write("Server (s) or client (c): ");
-			string choice = Console.ReadLine();
-			
-			IConnection connection;
-			DifferentialSynchronizationStrategy sync;
-			
-			
-			ITextContext context = new TextEditorTextContext(form.SourceCode);			
-			
-			if(choice == "s")
-			{
-				connection = new TcpServerConnection();	
-				
-				// We have to use a multichannel connection
-				MultichannelConnection mcc = new MultichannelConnection(connection);
-				sync = new DifferentialSynchronizationStrategy(0, context, mcc.CreateChannel());
-			
-				// Use 2nd channel to transport chat messages
-				var chatChannel = mcc.CreateChannel();		
-				form.SetChatConnection(chatChannel);
-			}
-			else
-			{
-				Console.Write("Address: ");
-				string address = Console.ReadLine();
-				try 
-				{
-					connection = new TcpClientConnection(address);
-				}
-				catch(ConnectionFailedException e)
-				{
-					Console.WriteLine("**ERROR** " + e.ToString());
-					Console.ReadKey(true);
-					return;
-				}
-				
-				// We have to use a multichannel connection
-				MultichannelConnection mcc = new MultichannelConnection(connection);
-				sync = new DifferentialSynchronizationStrategy(1, context, mcc.CreateChannel());
-			
-				// Use 2nd channel to transport chat messages
-				var chatChannel = mcc.CreateChannel();
-				form.SetChatConnection(chatChannel);
-			}
-			
-			
-			var timer = new System.Timers.Timer(20);
-			timer.Elapsed += delegate { 
-				clientForm.BeginInvoke((Action)UpdateText);
-			};
-			timer.Enabled = true;		*/	
-			
 			form.Show();	
 
 			
