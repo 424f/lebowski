3b0d24714dd4ff9108606d3887478b30e11f5aee
Willy Lai <laiw@student.ethz.ch>
2010-05-13 16:24:53 +0200

    reorganized tests

---
+++ Source/TwinEditor/Test/PythonInterpreterTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using IronPython;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using NUnit.Framework;
+
+
+namespace TwinEditor.Test
+{
+	[TestFixture]
+	public class PythonInterpreterTest
+	{
+		[Test]
+		public void testExecution()
+		{
+			var interpreter = new PythonInterpreter();
+			var writer = new PythonStringWriter();
+			object val = interpreter.ExecuteCode("print 'test'", writer);
+			Assert.AreEqual("test\n", writer.GetContent());
+		}		
+			
+		[Test]
+		public void testExecution2()
+		{
+			var interpreter = new PythonInterpreter();
+			var writer = new PythonStringWriter();
+			string code = @"
+def fact(n):
+  return (1 if n <= 1 else n*fact(n-1))
+for i in xrange(5):
+  print fact(i)";
+			object val = interpreter.ExecuteCode(code, writer);
+			Assert.AreEqual("1\n1\n2\n6\n24\n", writer.GetContent());
+		}
+		
+		[Test]
+		public void testErrorHandling1()
+		{
+			var interpreter = new PythonInterpreter();
+			var writer = new PythonStringWriter();
+			Assert.DoesNotThrow(delegate { interpreter.ExecuteCode("x", writer); });
+		}
+	}
+}
---
+++ Source/TwinEditor/Test/UITester.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TwinEditor.Test
+{
+	/// <summary>
+	/// Description of UITester.
+	/// </summary>
+	
+	[TestFixture]
+	public class UITester
+	{
+		
+		[STAThread]
+		[Test]
+		public void test1()
+		{
+			MainForm mainForm = new MainForm(new Controller());
+			mainForm.Show();
+			Assert.IsNotNull(mainForm);
+		}
+
+	}
+}
