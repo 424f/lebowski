65449b0eeddb2b260c840d0cdcfa83d79f059551
Boris Bluntschli <boris@bluntschli.ch>
2010-05-26 16:46:40 +0200

    Comments for {Accept, Decline}SharingInvitationMessage

--- Source/Lebowski/Source/Net/Skype/DeclineSharingInvitiationMessage.cs
+++ Source/Lebowski/Source/Net/Skype/DeclineSharingInvitiationMessage.cs
@@ -1,15 +1,26 @@
-
 namespace Lebowski.Net.Skype
 {
     using System;
+    
+    /// <summary>
+    /// Sent when a client rejects a SharingInvitationMessage using
+    /// the SkypeProtocol.
+    /// </summary>
     [Serializable]
     sealed class DeclineSharingInvitationMessage
     {
-        public int InvitationId { get; private set; }
-
+        /// <summary>
+        /// Initializes a new instance of the DeclineSharingInvitationMessage.
+        /// </summary>
+        /// <param name="invitationId">See <see cref="InvitationId">InvitationId</see>.</param>     
         public DeclineSharingInvitationMessage(int invitationId)
         {
             InvitationId = invitationId;
         }
+        
+        /// <summary>
+        /// An identifier that uniquely identifies the invitation that was rejected.
+        /// </summary>        
+        public int InvitationId { get; private set; }        
     }
 }
