db5e053df742b848716ae9b4b97cbf5f2263c9bd
Willy Lai <laiw@student.ethz.ch>
2010-05-27 00:10:07 +0200

    commented

--- Source/Lebowski/Source/TextModel/Operations/DeleteOperation.cs
+++ Source/Lebowski/Source/TextModel/Operations/DeleteOperation.cs
@@ -1,25 +1,30 @@
 namespace Lebowski.TextModel.Operations
 {
     /// <remarks>
-    /// This class encapsulates a insert operation.
-    /// A insert operation is defined by a text to insert and a position at which the text is to be inserted.
+    /// This class encapsulates a delete operation.
+    /// A insert operation is defined by the position of the character to be deleted.
     /// </remarks>
     public class DeleteOperation : TextOperation
     {
         /// <summary>
-        ///  Position where the deletion should take place
+        ///  The position of the character that should be deleted
         /// </summary>
         public int Position { get; set; }
         
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="position"></param>
+        /// <param name="position">The position of the character to be deleted</param>
         public DeleteOperation(int position)
         {
             Position = position;
         }
 
+        /// <summary>
+        /// Accepts
+        /// </summary>
+        /// <param name="visitor"></param>
+        /// <returns></returns>
         public override T Accept<T>(ITextOperationVisitor<T> visitor)
         {
             return visitor.VisitDeleteOperation(this);
