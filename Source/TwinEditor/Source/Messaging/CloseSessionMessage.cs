a4ad151d09db8abf3b52813a8ce3ba391706b4b8
Boris Bluntschli <boris@bluntschli.ch>
2010-05-27 01:04:23 +0200

    Commented TwinEditor.Messaging members

--- Source/TwinEditor/Source/Messaging/CloseSessionMessage.cs
+++ Source/TwinEditor/Source/Messaging/CloseSessionMessage.cs
@@ -2,9 +2,16 @@ namespace TwinEditor.Messaging
 {
     using System;
 
+    /// <summary>
+    /// Sent when a <see cref="TwinEditor.Sharing.SessionContext">SessionContext</see>
+    /// is closed or the session is closed for any other reason.
+    /// </summary>
     [Serializable]
     public sealed class CloseSessionMessage
     {
+        /// <summary>
+        /// Initializes a new instance of the CloseSessionMessage class.
+        /// </summary>
         public CloseSessionMessage()
         {
         }
