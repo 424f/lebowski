83c2fd305a7930c9bb9090b02d4c8515ab9f7ee7
Boris Bluntschli <boris@bluntschli.ch>
2010-05-27 21:26:13 +0200

    Added FileType tests

---
+++ Source/TwinEditorTests/Source/FileTypes/TextFileTypeTest.cs
@@ -0,0 +1,23 @@
+﻿namespace TwinEditorTests.FileTypes
+{
+    using NUnit.Framework;
+    using TwinEditor.FileTypes;
+    
+    [TestFixture]
+    public class TextFileTypeTest
+    {
+        IFileType fileType = new TextFileType();
+        
+        [Test]
+        public void CannotExecute()
+        {
+            Assert.AreEqual(false, fileType.CanExecute);
+        }
+
+        [Test]
+        public void CannotCompile()
+        {
+            Assert.AreEqual(false, fileType.CanCompile);
+        }                
+    }
+}
